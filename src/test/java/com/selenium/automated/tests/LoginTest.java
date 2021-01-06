54d56fb86e954a6f764af3a5ce68a3dd5130c215
mbacinska <monika.bacinska@empik.com>
2021-01-06 10:06:58 +0100

    login test

---
+++ src/test/java/com/selenium/automated/tests/LoginTest.java
@@ -0,0 +1,47 @@
+package com.selenium.automated.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+  @Test
+  public void loginTest() throws InterruptedException {
+
+    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe/");
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+
+    //open the page
+    String url = "https://the-internet.herokuapp.com/login";
+    driver.get(url);
+    //enter username
+    WebElement username = driver.findElement(By.id("username"));
+    username.sendKeys("tomsmith");
+    //enter password
+    WebElement password = driver.findElement(By.id("password"));
+    password.sendKeys("SuperSecretPassword!");
+    //click login button
+    WebElement submit = driver.findElement(By.xpath("//button[@type='submit']"));
+    submit.click();
+    //verifications
+    String expectedUrl = "https://the-internet.herokuapp.com/secure";
+    String actualUrl = driver.getCurrentUrl();
+    Assert.assertEquals(actualUrl,expectedUrl,"Actual page url is not the same as expected");
+
+    WebElement logoutButton = driver.findElement(By.xpath("//a[@class='button secondary radius']"));
+    Assert.assertTrue(logoutButton.isDisplayed(), "Logout button is not displayed");
+
+    String expectedMessage = "You logged into a secure area!";
+    WebElement actualMessage = driver.findElement(By.xpath("//div[@id='flash']"));
+    String messageText = actualMessage.getText();
+    Assert.assertTrue(messageText.contains(expectedMessage));
+
+    driver.quit();
+  }
+}
