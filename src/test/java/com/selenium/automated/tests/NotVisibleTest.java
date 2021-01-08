eeed641a74b9bd5281d907893a1b10da7f195abe
mbacinska <monika.bacinska@empik.com>
2021-01-08 15:41:53 +0100

    not visible element tests

---
+++ src/test/java/com/selenium/automated/tests/NotVisibleTest.java
@@ -0,0 +1,44 @@
+package com.selenium.automated.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NotVisibleTest {
+
+  @Test
+  public void notVisibleTest() throws InterruptedException {
+
+    //setUp
+    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe/");
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+
+    //open the page
+    String url = "http://the-internet.herokuapp.com/dynamic_loading/1";
+    driver.get(url);
+
+    //click start button
+    WebElement startButton = driver.findElement(By.xpath("//div[@id ='start']/button"));
+    startButton.click();
+
+    //verifications
+    String expectedMessage = "Hello World!";
+    WebElement actualMessage = driver.findElement(By.xpath("//div[@id='finish']"));
+
+    WebDriverWait wait = new WebDriverWait(driver, 5);
+    wait.until(ExpectedConditions.visibilityOf(actualMessage));
+    String messageText = actualMessage.getText();
+    Assert.assertTrue(messageText.contains(expectedMessage));
+
+    //close browser
+    driver.quit();
+
+  }
+}
